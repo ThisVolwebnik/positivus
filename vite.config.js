6c7d1dee347db3db0b79b9efc517c0fa9c2859f5
Nikita(ThisVolwebnik) <thisvolwebnik@yandex.ru>
2025-08-28 12:34:58 +0300

    feat: add favicon

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "ThisVolwebnik",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
